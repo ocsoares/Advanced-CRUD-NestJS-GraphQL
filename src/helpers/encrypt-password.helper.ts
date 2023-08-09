745bb4e34f3f648c5c5dc82df9143697031658a2
ocsoares <casoares016@gmail.com>
2023-08-08 21:13:41 -0300

    refactor: rename encrypt password helper variable

--- src/helpers/encrypt-password.helper.ts
+++ src/helpers/encrypt-password.helper.ts
@@ -2,8 +2,8 @@ import * as bcrypt from 'bcrypt';
 
 export class EncryptPasswordHelper {
     static async bcrypt(password: string, salt: number): Promise<string> {
-        const hashPassword = await bcrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-        return hashPassword;
+        return hashedPassword;
     }
 }
