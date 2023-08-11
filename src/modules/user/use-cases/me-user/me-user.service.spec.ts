0e1973a38ce42c9489af360021f8ef2d12f1f4a9
ocsoares <casoares016@gmail.com>
2023-08-11 18:23:16 -0300

    test: add me user service unit test

--- src/modules/user/use-cases/me-user/me-user.service.spec.ts
+++ src/modules/user/use-cases/me-user/me-user.service.spec.ts
@@ -1,18 +1,63 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { UserRepository } from '../../../../repositories/abstracts/UserRepository';
+import { TestUtilsCommon } from '../../../../common/test/test-utils.common';
+import { UserEntity } from '../../../../graphql/entities/user.entity';
+import { UserNotFoundException } from '../../../../exceptions/user-exceptions/user-not-found.exception';
 import { MeUserService } from './me-user.service';
 
 describe('MeUserService', () => {
-  let service: MeUserService;
+    let meUserService: MeUserService;
+    let userRepository: UserRepository;
+    let mockedUser: UserEntity;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MeUserService],
-    }).compile();
+    const mockUserRepository = TestUtilsCommon.mockUserRepository();
 
-    service = module.get<MeUserService>(MeUserService);
-  });
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                MeUserService,
+                {
+                    provide: UserRepository,
+                    useValue: mockUserRepository,
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+        meUserService = module.get<MeUserService>(MeUserService);
+        userRepository = module.get(UserRepository);
+
+        mockedUser = await TestUtilsCommon.newUser(true);
+    });
+
+    afterEach(() => {
+        Object.values(mockUserRepository).forEach((mockedMethod) =>
+            mockedMethod.mockReset(),
+        );
+    });
+
+    it('should be defined', () => {
+        expect(meUserService).toBeDefined();
+        expect(userRepository).toBeDefined();
+        expect(mockUserRepository).toBeDefined();
+        expect(mockedUser).toBeDefined();
+    });
+
+    it('should show the logged user', async () => {
+        mockUserRepository.findById.mockResolvedValue(mockedUser);
+
+        mockUserRepository.deleteOneById.mockResolvedValue(mockedUser);
+
+        const meUser = await meUserService.execute(mockedUser.id);
+
+        expect(meUser).toEqual(mockedUser);
+        expect(userRepository.findById).toHaveBeenCalledWith(mockedUser.id);
+    });
+
+    it(`should NOT show the logged user if the user doesn't exists with findById method`, async () => {
+        await expect(meUserService.execute(mockedUser.id)).rejects.toThrow(
+            new UserNotFoundException(),
+        );
+
+        expect(userRepository.findById).toHaveBeenCalledWith(mockedUser.id);
+    });
 });
--- src/modules/user/use-cases/me-user/me-user.service.ts
+++ src/modules/user/use-cases/me-user/me-user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { UserNotFoundException } from 'src/exceptions/user-exceptions/user-not-found.exception';
-import { UserEntity } from 'src/graphql/entities/user.entity';
-import { IService } from 'src/interfaces/IService';
-import { UserRepository } from 'src/repositories/abstracts/UserRepository';
+import { UserNotFoundException } from '../../../../exceptions/user-exceptions/user-not-found.exception';
+import { UserEntity } from '../../../../graphql/entities/user.entity';
+import { IService } from '../../../../interfaces/IService';
+import { UserRepository } from '../../../../repositories/abstracts/UserRepository';
 
 @Injectable()
 export class MeUserService implements IService {
