f558c008c2cc721d73902241fb6689be08a81ef2
ocsoares <casoares016@gmail.com>
2023-08-10 21:59:47 -0300

    test: add find user resolver unit test

--- src/modules/user/use-cases/find-user/find-user.resolver.spec.ts
+++ src/modules/user/use-cases/find-user/find-user.resolver.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserEntity } from 'src/graphql/entities/user.entity';
+import { TestUtilsCommon } from '../../../../common/test/test-utils.common';
+import { FindUserService } from './find-user.service';
 import { FindUserResolver } from './find-user.resolver';
 
-describe('FindUserResolver', () => {
-  let resolver: FindUserResolver;
+describe('FindUserService', () => {
+    let findUserResolver: FindUserResolver;
+    let findUserService: FindUserService;
+    let mockedUser: UserEntity;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [FindUserResolver],
-    }).compile();
+    const mockFindUserService = TestUtilsCommon.mockService();
 
-    resolver = module.get<FindUserResolver>(FindUserResolver);
-  });
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                FindUserResolver,
+                {
+                    provide: FindUserService,
+                    useValue: mockFindUserService,
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
-  });
+        findUserResolver = module.get<FindUserResolver>(FindUserResolver);
+        findUserService = module.get<FindUserService>(FindUserService);
+
+        mockedUser = await TestUtilsCommon.newUser(true);
+    });
+
+    afterEach(() => {
+        mockFindUserService.execute.mockReset();
+    });
+
+    it('should be defined', () => {
+        expect(findUserResolver).toBeDefined();
+        expect(findUserService).toBeDefined();
+        expect(mockFindUserService).toBeDefined();
+        expect(mockedUser).toBeDefined();
+    });
+
+    it('should find a user', async () => {
+        mockFindUserService.execute.mockResolvedValue(mockedUser);
+
+        const findUser = await findUserResolver.handle(mockedUser.id);
+
+        expect(findUser).toEqual(mockedUser);
+        expect(findUserService.execute).toHaveBeenCalledWith(mockedUser.id);
+    });
 });
--- src/modules/user/use-cases/find-user/find-user.resolver.ts
+++ src/modules/user/use-cases/find-user/find-user.resolver.ts
@@ -1,9 +1,9 @@
 import { Args, Query, Resolver } from '@nestjs/graphql';
 import { IResolver } from 'src/interfaces/IResolver';
 import { FindUserService } from './find-user.service';
-import { UserEntity } from 'src/graphql/entities/user.entity';
+import { UserEntity } from '../../../../graphql/entities/user.entity';
 import { UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
+import { JwtAuthGuard } from '../../../../modules/auth/guards/jwt-auth.guard';
 
 @Resolver()
 export class FindUserResolver implements IResolver {
